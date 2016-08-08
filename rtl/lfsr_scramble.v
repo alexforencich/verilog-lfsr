456a51c565d3958c95a7eb16ff9cf4acfd37460b
Alex Forencich <alex@alexforencich.com>
2016-08-08 06:53:56 -0700

    Add scrambler and descrambler modules and testbenches

---
+++ rtl/lfsr_scramble.v
@@ -0,0 +1,212 @@
+/*
+
+Copyright (c) 2016 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+// Language: Verilog 2001
+
+`timescale 1ns / 1ps
+
+/*
+ * LFSR scrambler
+ */
+module lfsr_scramble #
+(
+    // width of LFSR
+    parameter LFSR_WIDTH = 58,
+    // LFSR polynomial
+    parameter LFSR_POLY = 58'h8000000001,
+    // Initial state
+    parameter LFSR_INIT = {LFSR_WIDTH{1'b1}},
+    // LFSR configuration: "GALOIS", "FIBONACCI"
+    parameter LFSR_CONFIG = "FIBONACCI",
+    // bit-reverse input and output
+    parameter REVERSE = 1,
+    // width of data bus
+    parameter DATA_WIDTH = 64,
+    // implementation style: "AUTO", "LOOP", "REDUCTION"
+    parameter STYLE = "AUTO"
+)
+(
+    input  wire                  clk,
+    input  wire                  rst,
+    input  wire [DATA_WIDTH-1:0] data_in,
+    input  wire                  data_in_valid,
+    output wire [DATA_WIDTH-1:0] data_out
+);
+
+/*
+
+Fully parametrizable combinatorial parallel LFSR CRC module.  Implements an unrolled LFSR
+next state computation.  
+
+Ports:
+
+clk
+
+Clock input
+
+rst
+
+Reset module, set state to LFSR_INIT
+
+data_in
+
+Unscrambled data input (DATA_WIDTH bits)
+
+data_in_valid
+
+Shift input data through CRC when asserted
+
+data_out
+
+Scrambled data output (DATA_WIDTH bits)
+
+Parameters:
+
+LFSR_WIDTH
+
+Specify width of LFSR/CRC register
+
+LFSR_POLY
+
+Specify the LFSR/CRC polynomial in hex format.  For example, the polynomial
+
+x^32 + x^26 + x^23 + x^22 + x^16 + x^12 + x^11 + x^10 + x^8 + x^7 + x^5 + x^4 + x^2 + x + 1
+
+would be represented as
+
+32'h04c11db7
+
+Note that the largest term (x^32) is suppressed.  This term is generated automatically based
+on LFSR_WIDTH.
+
+LFSR_INIT
+
+Initial state of LFSR.  Defaults to all 1s.
+
+LFSR_CONFIG
+
+Specify the LFSR configuration, either Fibonacci or Galois.  Fibonacci is generally used
+for linear-feedback shift registers (LFSR) for pseudorandom binary sequence (PRBS) generators,
+scramblers, and descrambers, while Galois is generally used for cyclic redundancy check
+generators and checkers.
+
+Fibonacci style (example for 64b66b scrambler, 0x8000000001)
+
+    ,-----------------------------(+)<------------------------------,
+    |                              ^                                |
+    |  .----.  .----.       .----. |  .----.       .----.  .----.   |
+    `->|  0 |->|  1 |->...->| 38 |-+->| 39 |->...->| 56 |->| 57 |->(+)<-DIN (LSB first)
+       '----'  '----'       '----'    '----'       '----'  '----'
+
+Galois style (example for CRC16, 0x8005)
+
+    ,-------------------+---------------------------------+------------,
+    |                   |                                 |            |
+    |  .----.  .----.   V   .----.  .----.       .----.   V   .----.   |
+    `->|  0 |->|  1 |->(+)->|  2 |->|  3 |->...->| 14 |->(+)->| 15 |->(+)<-DIN (MSB first)
+       '----'  '----'       '----'  '----'       '----'       '----'
+
+REVERSE
+
+Bit-reverse LFSR input and output.
+
+DATA_WIDTH
+
+Specify width of the data bus.  The module will perform one shift per input data bit.
+
+STYLE
+
+Specify implementation style.  Can be "AUTO", "LOOP", or "REDUCTION".  When "AUTO"
+is selected, implemenation will be "LOOP" or "REDUCTION" based on synthesis translate
+directives.  "REDUCTION" and "LOOP" are functionally identical, however they simulate
+and synthesize differently.  "REDUCTION" is implemented with a loop over a Verilog
+reduction operator.  "LOOP" is implemented as a doubly-nested loop with no reduction
+operator.  "REDUCTION" is very fast for simulation in iverilog and synthesizes well in
+Quartus but synthesizes poorly in ISE, likely due to large inferred XOR gates causing
+problems with the optimizer.  "LOOP" synthesizes will in both ISE and Quartus.  "AUTO"
+will default to "REDUCTION" when simulating and "LOOP" for synthesizers that obey
+synthesis translate directives.
+
+Settings for common LFSR/CRC implementations:
+
+Name        Configuration           Length  Polynomial      Initial value   Notes
+CRC32       Galois, bit-reverse     32      32'h04c11db7    32'hffffffff    Ethernet FCS; invert final output
+PRBS6       Fibonacci               6       6'h21           any
+PRBS7       Fibonacci               7       7'h41           amy
+PRBS9       Fibonacci               9       9'h021          any             ITU V.52
+PRBS10      Fibonacci               10      10'h081         any             ITU
+PRBS11      Fibonacci               11      11'h201         any             ITU O.152
+PRBS15      Fibonacci               15      15'h4001        any             ITU O.152
+PRBS17      Fibonacci               17      17'h04001       any
+PRBS20      Fibonacci               20      20'h00009       any             ITU V.57
+PRBS23      Fibonacci               23      23'h040001      any             ITU O.151
+PRBS31      Fibonacci               31      31'h10000001    any
+64b66b      Fibonacci, bit-reverse  58      58'h8000000001  any             10G Ethernet
+128b130b    Galois, bit-reverse     23      23'h210125      any             PCIe gen 3
+
+*/
+
+// STATE_WIDTH is DATA_WIDTH or LFSR_WIDTH, whichever is larger
+parameter STATE_WIDTH = DATA_WIDTH > LFSR_WIDTH ? DATA_WIDTH : LFSR_WIDTH;
+
+reg [LFSR_WIDTH-1:0] state_reg = LFSR_INIT;
+reg [DATA_WIDTH-1:0] output_reg = 0;
+
+wire [STATE_WIDTH-1:0] lfsr_out;
+
+assign data_out = output_reg;
+
+lfsr #(
+    .LFSR_WIDTH(LFSR_WIDTH),
+    .LFSR_POLY(LFSR_POLY),
+    .LFSR_CONFIG(LFSR_CONFIG),
+    .REVERSE(REVERSE),
+    .DATA_WIDTH(DATA_WIDTH),
+    .OUTPUT_WIDTH(STATE_WIDTH),
+    .STYLE(STYLE)
+)
+lfsr_inst (
+    .data_in(data_in),
+    .lfsr_in(state_reg),
+    .lfsr_out(lfsr_out)
+);
+
+always @(posedge clk) begin
+    if (rst) begin
+        state_reg <= LFSR_INIT;
+        output_reg <= 0;
+    end else begin
+        if (data_in_valid) begin
+            if (REVERSE) begin
+                state_reg <= lfsr_out[STATE_WIDTH-1:STATE_WIDTH-LFSR_WIDTH];
+                output_reg <= lfsr_out[STATE_WIDTH-1:STATE_WIDTH-DATA_WIDTH];
+            end else begin
+                state_reg <= lfsr_out[LFSR_WIDTH-1:0];
+                output_reg <= lfsr_out[DATA_WIDTH-1:0];
+            end
+        end
+    end
+end
+
+endmodule
---
+++ tb/test_lfsr_scramble_64.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+"""
+
+Copyright (c) 2016 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+from myhdl import *
+import os
+import struct
+
+module = 'lfsr_scramble'
+testbench = 'test_lfsr_scramble_64'
+
+srcs = []
+
+srcs.append("../rtl/%s.v" % module)
+srcs.append("../rtl/lfsr.v")
+srcs.append("%s.v" % testbench)
+
+src = ' '.join(srcs)
+
+build_cmd = "iverilog -o %s.vvp %s" % (testbench, src)
+
+def dut_lfsr_scramble(clk,
+                      rst,
+                      current_test,
+                      data_in,
+                      data_in_valid,
+                      data_out):
+
+    if os.system(build_cmd):
+        raise Exception("Error running build command")
+    return Cosimulation("vvp -m myhdl %s.vvp -lxt2" % testbench,
+                clk=clk,
+                rst=rst,
+                current_test=current_test,
+                data_in=data_in,
+                data_in_valid=data_in_valid,
+                data_out=data_out)
+
+def scramble_64b66b(data, state=0x3ffffffffffffff):
+    data_out = bytearray()
+    for d in data:
+        b = 0
+        for i in range(8):
+            if bool(state & (1<<38)) ^ bool(state & (1<<57)) ^ bool(d & (1 << i)):
+                state = ((state & 0x1ffffffffffffff) << 1) | 1
+                b = b | (1 << i)
+            else:
+                state = (state & 0x1ffffffffffffff) << 1
+        data_out += bytearray([b])
+    return data_out
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+def bench():
+
+    # Parameters
+    LFSR_WIDTH = 58
+    LFSR_POLY = 0x8000000001
+    LFSR_INIT = 0x3ffffffffffffff
+    LFSR_CONFIG = "FIBONACCI"
+    REVERSE = 1
+    DATA_WIDTH = 64
+    STYLE = "AUTO"
+
+    # Inputs
+    clk = Signal(bool(0))
+    rst = Signal(bool(0))
+    current_test = Signal(intbv(0)[8:])
+
+    data_in = Signal(intbv(0)[DATA_WIDTH:])
+    data_in_valid = Signal(bool(0))
+
+    # Outputs
+    data_out = Signal(intbv(0)[DATA_WIDTH:])
+
+    # DUT
+    dut = dut_lfsr_scramble(clk,
+                            rst,
+                            current_test,
+                            data_in,
+                            data_in_valid,
+                            data_out)
+
+    @always(delay(4))
+    def clkgen():
+        clk.next = not clk
+
+    @instance
+    def check():
+        yield delay(100)
+        yield clk.posedge
+        rst.next = 1
+        yield clk.posedge
+        rst.next = 0
+        yield clk.posedge
+        yield delay(100)
+        yield clk.posedge
+
+        # testbench stimulus
+
+        yield clk.posedge
+        print("test 1: block")
+        current_test.next = 1
+
+        rst.next = 1
+        yield clk.posedge
+        rst.next = 0
+        yield clk.posedge
+
+        block = bytes(range(256))
+
+        scr = scramble_64b66b(block)
+
+        for i in range(0, len(block), 8):
+            data_in.next = struct.unpack('<Q', block[i:i+8])[0]
+            data_in_valid.next = 1
+            yield clk.posedge
+            if i > 0:
+                ref = struct.unpack('<Q', scr[i-8:i])[0]
+                print(hex(data_out))
+                print(hex(ref))
+                assert data_out == ref
+            i += 8
+        data_in_valid.next = 0
+        yield clk.posedge
+
+        yield delay(100)
+
+        raise StopSimulation
+
+    return dut, clkgen, check
+
+def test_bench():
+    sim = Simulation(bench())
+    sim.run()
+
+if __name__ == '__main__':
+    print("Running test...")
+    test_bench()
