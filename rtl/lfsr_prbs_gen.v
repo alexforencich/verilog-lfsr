97d7756e27e12afe31f62295c50a9a425a7e678e
Alex Forencich <alex@alexforencich.com>
2016-08-30 15:15:11 -0700

    Update implementation list

--- rtl/lfsr_prbs_gen.v
+++ rtl/lfsr_prbs_gen.v
@@ -154,15 +154,16 @@ Settings for common LFSR/CRC implementations:
 Name        Configuration           Length  Polynomial      Initial value   Notes
 CRC32       Galois, bit-reverse     32      32'h04c11db7    32'hffffffff    Ethernet FCS; invert final output
 PRBS6       Fibonacci               6       6'h21           any
-PRBS7       Fibonacci               7       7'h41           amy
+PRBS7       Fibonacci               7       7'h41           any
 PRBS9       Fibonacci               9       9'h021          any             ITU V.52
 PRBS10      Fibonacci               10      10'h081         any             ITU
 PRBS11      Fibonacci               11      11'h201         any             ITU O.152
-PRBS15      Fibonacci               15      15'h4001        any             ITU O.152
+PRBS15      Fibonacci, inverted     15      15'h4001        any             ITU O.152
 PRBS17      Fibonacci               17      17'h04001       any
 PRBS20      Fibonacci               20      20'h00009       any             ITU V.57
-PRBS23      Fibonacci               23      23'h040001      any             ITU O.151
-PRBS31      Fibonacci               31      31'h10000001    any
+PRBS23      Fibonacci, inverted     23      23'h040001      any             ITU O.151
+PRBS29      Fibonacci, inverted     29      29'h08000001    any
+PRBS31      Fibonacci, inverted     31      31'h10000001    any
 64b66b      Fibonacci, bit-reverse  58      58'h8000000001  any             10G Ethernet
 128b130b    Galois, bit-reverse     23      23'h210125      any             PCIe gen 3
 
